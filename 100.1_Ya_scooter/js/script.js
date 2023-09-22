c48d4d7cc9f8f9d3b59caa689ea1cdd6dcdb8738
Daniil <daniilbp@yandex.ru>
2023-09-22 22:47:12 +0400

    Ya_scooter

---
+++ 100.1_Ya_scooter/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	slidesPerGroup: 1,
+	spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next-2',
+    prevEl: '.swiper-button-prev-2',
+  }
+});
